881d4714986c6f5b688c61c14e43dcc058ada903
Eugegeuge sevilla martinez <hugosema.19@gmail.com>
2025-12-16 14:38:37 +0100

    refactor(css): centralize styles and config to assets/

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                slate: {
+                    900: '#0f172a', /* Fondo Principal Profile */
+                    800: '#1e293b', /* Tarjetas */
+                }
+            }
+        }
+    }
+}
